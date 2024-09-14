7eeb8fa21fab67d5a67b3770e0648cc42cdc9594
Roman Kudryavtsev <kudryavtsevroman@mail.ru>
2024-09-14 13:31:26 +0300

    initial commit

---
+++ basic/Area.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int length, width;
+    cout << "input length ";
+    cin >> length;
+    cout << "input width ";
+    cin >> width;
+    int area = length * width;
+    cout << "area:\n" << area;
+}
---
+++ basic/VarDemo.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int length;
+    length = 7;
+    cout << length;
+    return 0;
+}
